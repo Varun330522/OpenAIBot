4fa948bf3c5ceda08be7d7c355ea31af3c58844d
Varun330522 <53001278+Varun330522@users.noreply.github.com>
2022-12-27 20:42:42 +0530

    server updated

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://openaichatbot-fvj3.onrender.com', {
+    const response = await fetch('https://chatbotopenai.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
